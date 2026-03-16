76d03030d4ccd0d7d01d591569396b26e38ba967
TheSkyFuker <18301121008@163.com>
2026-03-16 23:56:16 +0800

    3-16 - 2道简单题

---
+++ CCF-CSP/2017/12/CSP17-12A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 1000;
+int n;
+int a[MAX];
+
+int dif = MAX * MAX;
+
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
+        {
+            if (abs(a[i] - a[j]) < dif)
+                dif = abs(a[i] - a[j]);
+        }
+
+    cout << dif;
+}
---
+++ CCF-CSP/2017/12/CSP17-12B.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 1000 + 1;
+
+int n, k;
+int a[MAX];
+
+int main()
+{
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i++)
+        a[i] = 1;
+
+    int cnt = n;
+    int num = 1;
+    int ptr = 0;
+    while (cnt != 1)
+    {
+        while (a[ptr] == 0)
+        {
+            ptr++;
+            ptr %= n;
+        }
+        if (num % k == 0 || num % 10 == k)
+        {
+            a[ptr] = 0;
+            cnt--;
+        }
+        num++;
+        ptr++;
+        ptr %= n;
+    }
+    for (int i = 0; i < n; i++)
+        if (a[i] != 0)
+            cout << i + 1;
+}
